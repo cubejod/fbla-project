3a5edceb01c08a3457128d1b2113f7af810d0099
cubejod <148727048+cubejod@users.noreply.github.com>
2023-12-15 16:18:34 -0500

    Update index.tsx

--- next-app/src/pages/admin/index.tsx
+++ next-app/src/pages/admin/index.tsx
@@ -1,6 +1,31 @@
+import React from 'react'
+
+const styles = {
+  container: {
+    display: 'flex' as 'flex',
+    flexDirection: 'column' as 'column',
+    alignItems: 'center' as 'center',
+    justifyContent: 'center' as 'center',
+    height: '100vh',
+    backgroundColor: '#f5f5f5',
+    color: '#333',
+    fontFamily: 'Arial, sans-serif',
+  },
+  welcome: {
+    fontSize: '2em',
+    fontWeight: 'bold' as 'bold',
+  },
+  message: {
+    marginTop: '1em',
+    fontSize: '1em',
+  },
+}
+
 const AdminPage: React.FC = () => {
   return (
-    <div>hello admin page!</div>
+    <div style={ styles.container }>
+      <div style={ styles.welcome }>Welcome, Admin!</div>
+    </div>
   )
 }
 
